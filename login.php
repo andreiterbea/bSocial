e8dbf08787cc5163b1ee4225721a2a1ec13fc91e
andreiterbea <andrei_tzb@yahoo.com>
2015-09-30 12:06:03 +0200

    Fixed login font

--- login.php
+++ login.php
@@ -18,14 +18,14 @@
 
   <section class="container">
     <div class="login">
-      <h1 style="font-family: auto;margin-left: 48px; font-size:33px; color:#349364;"> Login to Bsocial</h1>
+      <h1 style="font-family: 'autoradiographicrg'; margin-left: 48px; font-size:33px; color:#349364;"> Login to Bsocial</h1>
       <form method="post" action="index.html">
         <p><input type="text" name="login" value="" placeholder="Username"></p>
         <p><input type="password" name="password" value="" placeholder="Password"></p>
         <p class="remember_me">
           <label>
             <input type="checkbox" name="remember_me" id="remember_me">
-            Remember me on this computer
+            <p style="color:#000;">Remember me on this computer</p>
           </label>
         </p>
         <p class="submit" style="margin-left: 75px;"><input type="submit" name="commit" value="Login"></p>
