2adbaff2e86853920d03d815a3440174341d663c
CecilieMadsen <cillemode@hotmail.com>
2015-09-30 15:54:58 +0200

    calendar + index

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <head>
 <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <!-- Bootstrap -->
+    <link href="bootstrap/bootstrap.css" rel="stylesheet">
 
 <title>Front Page</title>
 
@@ -19,12 +23,42 @@
 <!-- Below is some PHP code that pulls the menu from another file (menu.php) and puts it on this page. This way it's easy to make changes and update every page at once -->
 <?php include 'include/menu.php';?>
 
+<div id="front_content">
 
-<div id="content">
+<div class="row">
+   <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <a href="groups.html"> <img src="billeder/groups.png" style="height:300px; width:300px;" alt="groups"> </a>
+      <div class="caption">
+        <div class="p2"> Create a new group or check out one of the groups you're a part of. <br/> </div>
+      </div>
+    </div>
+  </div>
+  
+  
+   <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <a href="calendar.html"> <img src="billeder/calendar.png" style="height:300px; width:300px;" alt="calendar"> </a>
+      <div class="caption">
+        <div class="p2">Remember to check your calendar, so you can keep track of all the cool events going on at Bsocial.<br/> </div>
+      </div>
+    </div>
+  </div>
+  
+  
+ 	  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <a href="notifications.html"> <img src="billeder/notifications.png" style="height:300px; width:300px;" alt="notifications"> </a>
+      <div class="caption">
+        <div class="p2">Don't forget to keep an eye out on your notifications. You don't wanna miss out on the fun stuff, right?
+        <br/> </div>
+      </div>
+    </div>
+  </div>
+ 
 
-
-
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script> 	
 
 
 
--- register.php
+++ register.php
@@ -4,18 +4,56 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
 <link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
-<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+<link rel="stylesheet" href="css/register.css"> <!-- Resource style -->
 <script src="js/modernizr.js"></script> <!-- Modernizr -->
 
-<title>Login</title>
+<title>Register</title>
 </head>
 
 <body>
+<wrapper>
+
+
+<div id="content">
+
+
+  <section class="container">
+    <div class="login">
+      <h1 style="font-family: 'autoradiographicrg'; margin-left: 48px; font-size:33px; color:#349364;"> Login to Bsocial</h1>
+      <form method="post" action="index.html">
+        <p><input type="text" name="login" value="" placeholder="Username"></p>
+        <p><input type="password" name="password" value="" placeholder="Password"></p>
+        <p class="remember_me">
+          <label>
+            <input type="checkbox" name="remember_me" id="remember_me">
+            <p style="color:#000;">Remember me on this computer</p>
+          </label>
+        </p>
+        <p class="submit" style="margin-left: 75px;"><input type="submit" name="commit" value="Login"></p>
+      </form>
+    </div>
+
+    <div class="login-help">
+      <p>Forgot your password? <a href="index.html">Click here to reset it</a>.</p>
+    </div>
+    
+    <div class="not-member" style="margin-left:400px;">
+      <p>Not a member? <a href="index.html">Click here to start connecting with your classmates</a>.</p>
+    </div>
+  </section>
+
+</div> <!-- content close -->
+
+
+
+
+
 <!-- Below is some PHP code that pulls the menu from another file (menu.php) and puts it on this page. This way it's easy to make changes and update every page at once -->
 <?php include 'include/menu.php';?>
 Register
 
 
 <?php include 'include/footer.php';?>
+</wrapper>
 </body>
 </html>
