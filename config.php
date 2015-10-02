6235918f014029ca9be75ccc0d8f8a6a3f82fa91
Bjørn Hvolbøll <blhmac@blhmacpro.local>
2015-10-02 11:06:03 +0200

    fixed config.php

--- config.php
+++ config.php
@@ -5,9 +5,9 @@ define("DB_NAME", "bsocial");
 define("DB_USER", "root");
 define("DB_PASSWORD", "root");
 
-define("USERS_TABLE_NAME", "users");
-define("GROUPS_TABLE_NAME", "groups");
-define("CHOSEN_GROUPS_TABLE_NAME", "chosen_groups");
-define("COMMENTS_TABLE_NAME", "comments");
-define("NOTIFICATIONS_TABLE_NAME", "notifications");
+define("USERS_TABLE_NAME", "bsocial_users");
+define("GROUPS_TABLE_NAME", "bsocial_groups");
+define("CHOSEN_GROUPS_TABLE_NAME", "bsocial_chosen_groups");
+define("COMMENTS_TABLE_NAME", "bsocial_comments");
+define("NOTIFICATIONS_TABLE_NAME", "bsocial_notifications");
 
